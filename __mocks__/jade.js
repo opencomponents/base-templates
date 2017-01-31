88c2d8ec44a8fcedf51dd8fc3140eb9554b469bd
Nick Balestra <nbalestra@opentable.com>
2017-01-31 14:15:05 +0000

    encapsuled jade hacks into module

--- __mocks__/jade.js
+++ __mocks__/jade.js
@@ -1,14 +1,7 @@
 /* eslint-disable no-underscore-dangle, no-var */
 
 const jade = jest.genMockFromModule('jade');
+const compileClient = jest.fn(() => '');
+jade.compileClient = compileClient;
 
-var linked = jest.fn();
-const __setTemplate = (fn) => {
-  linked = fn;
-};
-
-const template = jest.fn(() => linked);
-
-jade.__setTemplate = __setTemplate;
-jade.template = template;
 module.exports = jade;
--- src/precompile.js
+++ src/precompile.js
@@ -1,3 +1,14 @@
 const jade = require('jade');
+const assign = require('lodash.assign');
 
-module.exports = (template, options) => jade.compileClient(template, options);
+const defaults = {
+  compileDebug: false,
+  name: 't'
+};
+
+module.exports = (template, options) => {
+  const opts = assign({}, defaults, options);
+  return jade.compileClient(template, opts)
+    .toString()
+    .replace('function t(locals) {', 'function(locals){');
+};
