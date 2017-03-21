1abea6786fbb73b84581f076900a06ab2f237d31
Nick Balestra <nbalestra@opentable.com>
2017-03-21 14:51:53 +0000

    err, res calbback

--- src/compile.js
+++ src/compile.js
@@ -1,13 +1,19 @@
+/* eslint-disable no-var */
+
 const jade = require('jade');
 
-module.exports = (options, callback) =>
-  callback(
-    jade.compileClient(options.template, {
+module.exports = (options, callback) => {
+  var compiled;
+  try {
+    compiled = jade.compileClient(options.template, {
       compileDebug: false,
       name: 't',
       filename: options.viewPath
     })
     .toString()
-    .replace('function t(locals) {', 'function(locals){')
-  );
-
+    .replace('function t(locals) {', 'function(locals){');
+  } catch (err) {
+    return callback(err);
+  }
+  return callback(null, compiled);
+};
