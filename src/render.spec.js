bf881d70c85c6c7b02eb675b7757c572fe6c6d99
Matteo Figus <matteofigus@gmail.com>
2017-05-23 11:54:15 +0100

    Need to catch the exception during the render

--- src/render.spec.js
+++ src/render.spec.js
@@ -1,19 +1,27 @@
 const render = require('./render');
 
-const template = '<div>Hello</div>';
-const precompiledTemplate = jest.fn(() => '<div>Hello</div>');
-const options = {
-  model: 'aModel',
-  template: precompiledTemplate
-};
-const callback = jest.fn();
-
 describe('render method', () => {
   describe('when invoked with a template', () => {
-    render(options, callback);
+    const model = { aModel: true };
+    const template = () => '<div>Hello</div>';
+    const callback = jest.fn();
+
+    render({ model, template }, callback);
 
     test('should correctly invoke the callback', () => {
-      expect(callback).toBeCalledWith(null, template);
+      expect(callback).toBeCalledWith(null, '<div>Hello</div>');
+    });
+  });
+
+  describe('when invoked with a broken view-model that throws an exception', () => {
+    const model = { aModel: true };
+    const template = () => { throw new Error('blargh'); };
+    const callback = jest.fn();
+
+    render({ model, template }, callback);
+
+    test('should correctly invoke the callback', () => {
+      expect(callback).toBeCalledWith(new Error('blargh'));
     });
   });
 });
