e83614907b9e8f97f413b25dc98d127c5b2fa6be
Nick Balestra <nbalestra@opentable.com>
2017-06-19 12:26:15 +0100

    oc-view-wrapper module

--- packages/oc-view-wrapper/index.js
+++ packages/oc-view-wrapper/index.js
@@ -1,6 +1,4 @@
 'use strict';
 
-const format = require('stringformat');
-
 module.exports = (hash, content, nameSpace = 'oc') =>
   `var ${nameSpace}=${nameSpace}||{};${nameSpace}.components=${nameSpace}.components||{};${nameSpace}.components['${hash}']=${content}`;
