315cc0c9490a2abb7460e4b2b8d5221e6c8a892b
Nick Balestra <nbalestra@opentable.com>
2017-06-20 16:36:37 +0100

    moar test for the external deps handler

--- packages/oc-external-dependencies-handler/test/oc-external-dependencies-handler.test.js
+++ packages/oc-external-dependencies-handler/test/oc-external-dependencies-handler.test.js
@@ -1,8 +1,14 @@
 const externalDependenciesHandler = require('../index.js');
 
+test('The handler API once instiantated', () => {
+  const handler = externalDependenciesHandler({});
+  expect(handler.length).toBe(2);
+});
+
 test('When a module is being used withough being declared in the package', done => {
   const handler = externalDependenciesHandler({});
   const handlerFunction = handler[0];
+  expect(handlerFunction).toBeInstanceOf(Function);
   handlerFunction(null, 'lodash', err => {
     expect(err).toMatchSnapshot();
     done();
@@ -21,8 +27,11 @@ test('When a module declared in the package is being used', done => {
 test('The handler matcher should correctly match aganinst valid modules', () => {
   const handler = externalDependenciesHandler({ lodash: '4.17.4' });
   const handlerMatcher = handler[1];
+  expect(handlerMatcher).toBeInstanceOf(RegExp);
   expect(handlerMatcher.test('lodash')).toBe(true);
   expect(handlerMatcher.test('yet-another-module')).toBe(true);
   expect(handlerMatcher.test('@org/module-name')).toBe(true);
   expect(handlerMatcher.test('@org/module/path')).toBe(true);
+  expect(handlerMatcher.test('/myModule')).toBe(false);
+  expect(handlerMatcher.test('./myModule')).toBe(false);
 });
