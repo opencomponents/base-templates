97deb4247dd06fd1a8efa7b547ac209705e036fc
Matteo Figus <matteofigus@gmail.com>
2017-07-13 23:57:01 +0100

    Added some tests

--- packages/oc-minify-file/test/oc-minify-file.test.js
+++ packages/oc-minify-file/test/oc-minify-file.test.js
@@ -10,3 +10,18 @@ test('Minify not valid js', () => {
   const content = 'const a=notvalid(';
   expect(minifyFile.bind(null, '.js', content)).toThrowErrorMatchingSnapshot();
 });
+
+test('Minify valid css file', () => {
+  const content = `body {
+    background-color: #FFFFFF;
+  }`;
+
+  expect(minifyFile('.css', content)).toMatchSnapshot();
+});
+
+test('Minify not valid css', () => {
+  const content = `body {
+    background-color: #hello`;
+
+  expect(minifyFile('.css', content)).toThrowErrorMatchingSnapshot();
+});
