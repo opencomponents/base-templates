69e6dc5e1d4021f372f68e1622c6ea7ef0d4a043
Nick Balestra <nbalestra@opentable.com>
2017-07-23 15:47:21 +0100

    chore(scripts): avoid runinng code-coverage twice

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  npm run coverage
-fi
-
 if [ $TRAVIS_BRANCH == 'master' ]; then
   if [ $TRAVIS_NODE_VERSION == '6' ]; then
     npm run semantic-release
