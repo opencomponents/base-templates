93d6edcb8dc332ec085f8007918a5c5bb594a8f6
Nick Balestra <nbalestra@opentable.com>
2017-08-23 11:17:17 +0100

    fix(getinfo): expose original name in externals
    affects: oc-generic-template-renderer

--- packages/oc-generic-template-renderer/lib/getInfo.js
+++ packages/oc-generic-template-renderer/lib/getInfo.js
@@ -1,7 +1,9 @@
 module.exports = packageJson => {
-  const externals = Object.keys(packageJson.externals).map(
-    dep => packageJson.externals[dep]
-  );
+  const externals = Object.keys(packageJson.externals).map(dep => {
+    const ext = packageJson.externals[dep];
+    ext.name = dep;
+    return ext;
+  });
 
   return {
     type: packageJson.name,
