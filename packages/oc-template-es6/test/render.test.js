119e9daffcf348e205dec032757c97bd9613c31c
Nick Balestra <nbalestra@opentable.com>
2018-02-08 15:39:07 +0000

    wip

--- packages/oc-template-es6/test/render.test.js
+++ packages/oc-template-es6/test/render.test.js
@@ -2,26 +2,26 @@ const { render } = require('../index.js');
 
 describe('render method', () => {
   describe('when invoked with a valid template', () => {
-    const model = { aModel: true };
-    const template = () => '<div>Hello</div>';
+    const model = { name: 'JavaScript' };
+    const template = props => `<div>Hello ${props.name.toUpperCase()}</div>`;
     const callback = jest.fn();
 
     render({ model, template }, callback);
     test('should correctly invoke the callback', () => {
-      expect(callback).toBeCalledWith(null, '<div>Hello</div>');
+      expect(callback).toBeCalledWith(null, '<div>Hello JAVASCRIPT</div>');
     });
   });
 
   describe('when invoked with a broken view-model that throws an exception', () => {
-    const model = { aModel: true };
-    const template = () => {
-      throw new Error('blargh');
-    };
+    const model = { name: 'JavaScript' };
+    const template = props => `<div>Hello ${blargh}</div>`;
     const callback = jest.fn();
 
     render({ model, template }, callback);
     test('should correctly invoke the callback', () => {
-      expect(callback).toBeCalledWith(new Error('blargh'));
+      expect(callback).toBeCalledWith(
+        new ReferenceError('blargh is not defined')
+      );
     });
   });
 });
