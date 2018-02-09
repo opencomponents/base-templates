2c5ad9d6162c9d423dc38922c0827cd56dc12021
Nick Balestra <nbalestra@opentable.com>
2018-02-09 12:34:26 +0000

    oc-template-es6 compiler  wip

---
+++ packages/oc-template-es6-compiler/README.md
@@ -0,0 +1,3 @@
+# oc-template-es6-compiler - Compiler module
+
+OC template to compile components of type `oc-template-es6`
---
+++ packages/oc-template-es6-compiler/test/compile.test.js
@@ -0,0 +1,44 @@
+// jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
+
+// const path = require('path');
+// const compile = require('../lib/compile.js');
+// const fs = require('fs-extra');
+// const nodeDir = require('node-dir');
+
+// const componentPackage = require('../../../mocks/jade-component/package.json');
+// const componentPath = path.join(__dirname, '../../../mocks/jade-component/');
+// const getInfo = require('../index.js').getInfo;
+// const publishPath = path.join(componentPath, '__package');
+
+// test('Should correctly compile the oc component', done => {
+//   const options = {
+//     componentPackage,
+//     ocPackage: {
+//       version: '1.0.0'
+//     },
+//     componentPath,
+//     publishPath,
+//     production: true
+//   };
+//   fs.ensureDirSync(publishPath);
+//   compile(options, (err, res) => {
+//     expect(err).toBeNull();
+//     const version = res.oc.files.template.version;
+//     res.oc.files.template.version = '';
+//     res.oc.date = '';
+//     expect(res).toMatchSnapshot();
+//     expect(version).toBe(getInfo().version);
+//     nodeDir.paths(publishPath, (err, res) => {
+//       const files = res.files
+//         .map(filePath => path.relative(__dirname, filePath))
+//         .sort();
+//       expect(files).toMatchSnapshot();
+//       fs.emptyDirSync(publishPath);
+//       done();
+//     });
+//   });
+// });
+
+test('', () => {
+  console.log('// compile.test');
+});
