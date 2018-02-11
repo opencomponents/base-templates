322a781ee8c9369fe0304b943faca818a6f4113c
Nick Balestra <nbalestra@opentable.com>
2018-02-11 14:35:39 +0000

    safer getInfo for the basic-template-renderer module

--- packages/oc-generic-template-renderer/test/getInfo.test.js
+++ packages/oc-generic-template-renderer/test/getInfo.test.js
@@ -1,20 +1,31 @@
 const getInfo = require('../lib/getInfo');
-const packageJson = {
-  name: 'oc-template-generic',
-  version: '1.0.0',
-  externals: {
-    'lib-one': {
-      global: 'LibOne',
-      url: 'https://cdn.com/lib-one/1.0.0/lib-one.min.js'
-    },
-    'lib-two': {
-      global: 'LibTwo',
-      url: 'https://cdn.com/lib-two/1.0.0/lib-two.min.js'
-    }
-  }
-};
 
 test('should return the correct info', () => {
+  const packageJson = {
+    name: 'oc-template-generic',
+    version: '1.0.0',
+    externals: {
+      'lib-one': {
+        global: 'LibOne',
+        url: 'https://cdn.com/lib-one/1.0.0/lib-one.min.js'
+      },
+      'lib-two': {
+        global: 'LibTwo',
+        url: 'https://cdn.com/lib-two/1.0.0/lib-two.min.js'
+      }
+    }
+  };
+
   const info = getInfo(packageJson);
   expect(info).toMatchSnapshot();
+});
+
+test('should return an an empty array if the template does not specify any externals', () => {
+  const packageJson = {
+    name: 'oc-template-generic',
+    version: '1.0.0'
+  };
+
+  const info = getInfo(packageJson);
+  expect(info.externals).toEqual([]);
 });
